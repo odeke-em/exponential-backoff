dba1e45298dec02d6e6f0fba2beab11b7d0c4625
Emmanuel Odeke <emm.odeke@gmail.com>
2015-05-24 01:05:54 -0600

    expb: sync in

--- expb/main.go
+++ expb/main.go
@@ -2,47 +2,16 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	expb "github.com/odeke-em/exponential-backoff"
 )
-
-func tryGet(uri string) expb.Producer {
-	return func() (interface{}, error) {
-		return http.Get(uri)
-	}
-}
 
 func consume(result interface{}, err error) {
 	fmt.Printf("result: %v err: %v\n", result, err)
 }
 
-func newBackoff(url string, retryCount uint32) *expb.ExponentialBacker {
-	req := tryGet(url)
-	return &expb.ExponentialBacker{
-		Do:          req,
-		StatusCheck: httpStatus,
-		RetryCount:  retryCount,
-	}
-}
-
-func httpStatus(v interface{}) (ok, retryable bool) {
-	res := v.(*http.Response)
-	statusCode := res.StatusCode
-	fmt.Println("statuscode", statusCode)
-	if statusCode >= 200 && statusCode <= 299 {
-		ok = true
-		return
-	}
-	if statusCode == http.StatusForbidden {
-		retryable = true
-		return
-	}
-	return
-}
-
 func main() {
-	backer := newBackoff("https://golang.org/pkg/net/httpx", 5)
+	backer := expb.NewUrlGetter("https://golang.org/pkg/net/httpx", 5)
 	expb.ExponentialBackOff(backer, consume)
 	fmt.Println("expb")
 }
